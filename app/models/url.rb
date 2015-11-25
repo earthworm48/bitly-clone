56f5e1ed0d438321f87756c70667461ebd9a9115
agweiler <kheryee@live.com>
2015-11-25 11:21:09 +0800

    generate 6 letters string for short url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,13 @@
+require_relative '../../config/environments/test'
+
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
+  def self.shorten
+    Array.new(6){[*"A".."Z", *"0".."9"].sample}.join
+  end
 end
+
+p Url.shorten
+
+
+
